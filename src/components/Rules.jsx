f249477e98641dcb447611b0aa7020bc1cddd99e
Aditya Babu Kesharwani <108403658+adityakesh@users.noreply.github.com>
2024-09-13 20:03:47 +0530

    Update Rules.jsx

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -9,7 +9,7 @@ export default function Rules() {
             <p>Select any number</p>
             <p>Click on dice image</p>
             <p>After click on  dice  if selected number is equal to dice number you will get same point as dice </p>
-            <p>If you get wrong guess then  2 point will be dedcuted </p>
+            <p>If you get wrong guess then  1 point will be dedcuted </p>
         </div>
     </RulesContainer>
   )
